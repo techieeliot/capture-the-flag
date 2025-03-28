cbacf4703b991317540b0ef2516a2098d181ef97
Eliot Sanford <techieeliot@gmail.com>
2025-03-28 07:38:51 +0000

    allow preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["nyx9n8-5173.csb.app"],
+  },
+});
